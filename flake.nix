f369bc8fb20b25be88b669719d2d5a6c175f6209
ogadra <61941819+ogadra@users.noreply.github.com>
2026-03-19 22:05:32 +0900

    Add terraform, tflint, and checkov to flake.nix and lefthook

--- flake.nix
+++ flake.nix
@@ -6,14 +6,21 @@
   outputs = { nixpkgs, ... }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfreePredicate = pkg:
+          builtins.elem (pkgs.lib.getName pkg) [ "terraform" ];
+      };
     in
     {
       devShells.${system}.default = pkgs.mkShell {
         packages = with pkgs; [
+          checkov
           gitleaks
           go_1_26
           lefthook
+          terraform
+          tflint
           trivy
         ];
       };
