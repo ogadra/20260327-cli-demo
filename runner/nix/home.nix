b81158afd401cefce8802b0ac741b3c3c71cf66d
ogadra <61941819+ogadra@users.noreply.github.com>
2026-03-27 08:33:00 +0900

    add container config file

---
+++ runner/nix/home.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+{
+  home.username = "root";
+  home.homeDirectory = "/root";
+  home.stateVersion = "25.11";
+  home.enableNixpkgsReleaseCheck = false;
+  
+  home.sessionVariables = {
+    TZ = "Asia/Tokyo";
+    LANG = "ja_JP.UTF-8";
+    LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+    TZDIR = "${pkgs.tzdata}/share/zoneinfo";
+  };
+
+  programs.bash.enable = true;
+}
